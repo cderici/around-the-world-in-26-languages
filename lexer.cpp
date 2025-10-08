2f7e1d94518ffc5c918779a6c237764fa4cc7af6
Caner Derici <caner@dericilab.live>
2025-10-08 13:35:20 -0600

    feat(lexer): start lexer and parsing

---
+++ lexer.cpp
@@ -0,0 +1,72 @@
+#include <cctype>
+#include <cstdlib>
+#include <string>
+
+enum Token {
+  tok_eof = -1,
+
+  tok_def = -2,
+  tok_extern = -3,
+
+  tok_identifier = -4,
+  tok_number = -5,
+};
+
+static std::string IdentifierStr;
+static double NumVal;
+
+// the actual lexer
+static int gettok() {
+  // LastChar: read, but not processed
+  static int LastChar = ' ';
+
+  // Skip whitespace
+  while (isspace(LastChar))
+    LastChar = getchar();
+
+  // identifier
+  if (isalpha(LastChar)) {
+    IdentifierStr = LastChar;
+
+    while (std::isalnum((LastChar = getchar())))
+      IdentifierStr += LastChar;
+
+    if (IdentifierStr == "def")
+      return tok_def;
+    if (IdentifierStr == "extern")
+      return tok_extern;
+    return tok_identifier;
+  }
+
+  // numbers
+  if (isdigit(LastChar) || LastChar == '.') {
+    std::string NumStr;
+
+    do {
+      NumStr += LastChar;
+      LastChar = getchar();
+    } while (isdigit(LastChar) || LastChar == '.');
+
+    NumVal = std::strtod(NumStr.c_str(), 0);
+    return tok_number;
+  }
+
+  // comments
+  if (LastChar == '#') {
+    do {
+      LastChar = getchar();
+    } while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+    if (LastChar != EOF)
+      return gettok();
+  }
+
+  // check EOF
+  if (LastChar == EOF)
+    return tok_eof;
+
+  // return char with ascii value
+  int ThisChar = LastChar;
+  LastChar = getchar();
+  return ThisChar;
+}
