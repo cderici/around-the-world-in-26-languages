81af61fbbadedaedb8c666ed5bf9f7213714ba5f
Caner Derici <caner@dericilab.live>
2025-10-13 15:58:04 -0600

    feat(ast): start working on the ast and the parser

---
+++ ast.cpp
@@ -0,0 +1,77 @@
+#include <memory>
+#include <string>
+#include <vector>
+
+// base class for expressions
+class ExprAST {
+public:
+  virtual ~ExprAST() = default;
+};
+
+// Numbers
+class NumberExprAST : public ExprAST {
+  double Val;
+
+public:
+  NumberExprAST(double v) : Val(v) {}
+};
+
+// Vars
+class VariableExprAST : public ExprAST {
+  std::string Name;
+
+public:
+  VariableExprAST(const std::string &Name) : Name(Name) {}
+};
+
+// Binops
+class BinaryExprAST : public ExprAST {
+  char Op;
+  std::unique_ptr<ExprAST> LHS, RHS;
+
+public:
+  BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS,
+                std::unique_ptr<ExprAST> RHS)
+      : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+};
+
+// CallExprAST - function calls
+class CallExprAST : public ExprAST {
+  std::string Callee;
+  std::vector<std::unique_ptr<ExprAST>> Args;
+
+public:
+  CallExprAST(const std::string &Callee,
+              std::vector<std::unique_ptr<ExprAST>> Args)
+      : Callee(Callee), Args(std::move(Args)) {}
+};
+
+// PrototypeAST - represents a prototype for a function (name + argnames). Note
+// that the language we're implementing doesn't have static types.
+class PrototypeAST {
+  std::string Name;
+  std::vector<std::string> Args;
+
+public:
+  PrototypeAST(std::string Name, std::vector<std::string> Args)
+      : Name(Name), Args(std::move(Args)) {}
+
+  const std::string &getName() const { return Name; }
+};
+
+// FunctionAST - This represents a lambda.
+class FunctionAST {
+  std::unique_ptr<PrototypeAST> Proto;
+  std::unique_ptr<ExprAST> Body;
+
+public:
+  FunctionAST(std::unique_ptr<PrototypeAST> Proto,
+              std::unique_ptr<ExprAST> Body)
+      : Proto(std::move(Proto)), Body(std::move(Body)) {}
+};
+
+// CurTok/getNextToken - provide a simple token buffer. Curtok is the current
+// token the parser is looking at. getNextToken reads another token from the
+// lexer and updates CurTok with its results.
+static int CurTok;
+static int getNextToken() {}
