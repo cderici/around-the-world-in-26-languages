58fdcec754e046f990c62af31cbce2356a4a1cb8
Caner Derici <caner@dericilab.live>
2026-03-09 16:18:05 -0600

    feat(frontend::lex): consumeTrivia in lexer engine

--- shared/frontend/lex/src/lexer.cpp
+++ shared/frontend/lex/src/lexer.cpp
@@ -2,6 +2,7 @@
 #include "token.h"
 #include <cctype>
 #include <string_view>
+#include <vector>
 
 namespace frontend::lex {
 
@@ -9,6 +10,12 @@ Lexer::Lexer(CharStream &cs, const ILexLanguageRules &langLexConfig)
     : cs_(cs), langLexConfig_(langLexConfig) {}
 
 // invariant: next always consumes at least one char unless EOF
+//
+// Trivia flow:
+// - Start of next, we clear the trivia
+// - Then consumeTrivia fills trivia_
+// - next() returns the non-trivia token
+// - leaadingTrivia refers to that token's leading trivia.
 Token Lexer::next() {
   // clear trivia from previous token
   trivia_.clear();
@@ -41,12 +48,71 @@ Token Lexer::next() {
   }
 }
 
+const std::vector<TriviaPiece> &Lexer::leadingTrivia() const { return trivia_; }
+
+void Lexer::consumeTrivia() {
+  while (!cs_.eof()) {
+    char c = cs_.peek();
+
+    if (c == '\n') {
+      const std::size_t start_offset = cs_.offset();
+      const std::size_t start_line = cs_.line();
+      const std::size_t start_column = cs_.column();
+
+      cs_.get();
+
+      const std::size_t end_offset = cs_.offset();
+      trivia_.push_back(TriviaPiece{
+          TriviaKind::Newline,
+          cs_.view(start_offset, end_offset),
+          SourceLoc{
+              .start_offset = start_offset,
+              .end_offset = end_offset,
+              .start_line = start_line,
+              .start_column = start_column,
+              .end_line = cs_.line(),
+              .end_column = cs_.column(),
+          },
+      });
+      continue;
+    }
+
+    if (std::isspace(static_cast<unsigned char>(c))) {
+      const std::size_t start_offset = cs_.offset();
+      const std::size_t start_line = cs_.line();
+      const std::size_t start_column = cs_.column();
+
+      while (!cs_.eof()) {
+        const char wc = cs_.peek();
+        if (wc == '\n' || !std::isspace(static_cast<unsigned char>(wc)))
+          break;
+        cs_.get();
+      }
+
+      const std::size_t end_offset = cs_.offset();
+      trivia_.push_back(TriviaPiece{
+          TriviaKind::Whitespace,
+          cs_.view(start_offset, end_offset),
+          SourceLoc{
+              .start_offset = start_offset,
+              .end_offset = end_offset,
+              .start_line = start_line,
+              .start_column = start_column,
+              .end_line = cs_.line(),
+              .end_column = cs_.column(),
+          },
+      });
+      continue;
+    }
+
+    break;
+  }
+}
+
 // invariant must be kept for lexing functions, always consume at least one char
 // unless EOF
 
 /*
-
-const std::vector<TriviaPiece> &leadingTrivia() const;
 
 private:
 void consumeTrivia();
